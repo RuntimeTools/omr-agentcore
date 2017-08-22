8e64e018514336d2d37acdd2a41ec16efcf79b7f
sjanuary <sjanuary@uk.ibm.com>
2017-08-22 14:44:51 +0100

    rename package back to omr-agentcore

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ import PackageDescription
 #endif
 
 let package = Package(
-    name: "SwiftMetricsCore",
+    name: "omr-agentcore",
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
 
