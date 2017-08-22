94c98ba6ac76a361ec716a9fa37111f863d25dc3
sjanuary <sjanuary@uk.ibm.com>
2017-08-22 15:24:12 +0100

    Update comments in Package.swift

--- Package.swift
+++ Package.swift
@@ -1,5 +1,5 @@
 // swift-tools-version:4.0
-// The swift-tools-version declares the minimum version of Swift required to build this package.
+// omr-agentcore Swift 4 package
 
 import PackageDescription
 
@@ -12,8 +12,7 @@ import PackageDescription
 let package = Package(
     name: "omr-agentcore",
     products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
-
+        // omr-agentcore libraries.
         .library(name: "agentcore", type: .dynamic, targets: ["agentcore"]),
         .library(name: "hcapiplugin", type: .dynamic, targets: ["hcapiplugin"]),
         .library(name: "memplugin", type: .dynamic, targets: ["memplugin"]),
@@ -22,18 +21,14 @@ let package = Package(
         .library(name: "mqttplugin", type: .dynamic, targets: ["mqttplugin"]),
         .library(name: "paho", type: .dynamic, targets: ["paho"])
     ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-    ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+        // omr-agentcore targets, one for each library.
       .target(name: "agentcore",
           exclude: [ "ibmras/common/port/aix",
                      "ibmras/common/port/windows",
                      "ibmras/common/data",
                      "ibmras/common/util/memUtils.cpp",
+                     "ibmras/monitoring/connector/headless",
                      excludePortDir
       ]),
       .target(name: "paho",
@@ -46,7 +41,7 @@ let package = Package(
               "src/SSLSocket.c",
               "src/samples",
       ]),
-      .target(name: "mqttplugin", dependencies: ["paho", "agentcore"]),
+      .target(name: "mqttplugin", dependencies: ["paho"]),
       .target(name: "cpuplugin"),
       .target(name: "envplugin"),
       .target(name: "memplugin"),
